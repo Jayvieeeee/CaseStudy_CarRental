aceb5551822abb2012afcd6ff0d9666efee674d3
ann <dalmaciochrisha6@gmail.com>
2025-03-05 02:05:05 +0800

    added databases

---
+++ CarRental/Controllers/CarController.cs
@@ -0,0 +1,24 @@
+﻿using CarRental.Data;
+using CarRental.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Linq;
+
+namespace CarRental.Controllers
+{
+    public class CarController : Controller
+    {
+        private readonly AppDbContext _context;
+
+        public CarController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult Index()
+        {
+            var cars = _context.Cars.ToList(); 
+            return View(cars);
+        }
+    }
+}
